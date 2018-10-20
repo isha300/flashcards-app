331a1e85b30f7a2107f576cd536f8d5670132588
Isha Kabra <isha.kabra10@gmail.com>
2018-10-20 13:40:53 -0400

    Add listeners to indicate if question was answered correctly by changing background colors of answers

--- app/src/main/java/com/codepath/flashcards/MainActivity.java
+++ app/src/main/java/com/codepath/flashcards/MainActivity.java
@@ -14,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView questionText = findViewById(R.id.flashcard_question);
         final TextView answerText = findViewById(R.id.flashcard_answer);
+        final TextView choice1 = findViewById(R.id.choice1);
+        final TextView choice2 = findViewById(R.id.choice2);
+        final TextView choice3 = findViewById(R.id.choice3);
 
         questionText.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -28,5 +31,27 @@ public class MainActivity extends AppCompatActivity {
                 questionText.setVisibility(View.VISIBLE);
             }
         });
+
+        choice1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                choice1.setBackgroundColor(getResources().getColor(R.color.my_red_color, null));
+                choice3.setBackgroundColor(getResources().getColor(R.color.my_green_color, null));
+            }
+        });
+
+        choice2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                choice1.setBackgroundColor(getResources().getColor(R.color.my_red_color, null));
+                choice3.setBackgroundColor(getResources().getColor(R.color.my_green_color, null));
+            }
+        });
+
+        choice3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                choice1.setBackgroundColor(getResources().getColor(R.color.my_red_color, null));
+                choice3.setBackgroundColor(getResources().getColor(R.color.my_green_color, null));
+            }
+        });
+
     }
 }
